7bb35c959e858c2c0f2b7eff870a978a75c7aea4
Abhinav Gopalakrishnan <abhi67devil@gmail.com>
2025-08-03 22:57:02 +0530

    update main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ while True:
     #Detect faces
     face_locations,face_names = sfr.detect_known_faces(frame)
     for face_loc,name in zip(face_locations,face_names):
-        y1,x1,y2,x2=face_loc[0],face_loc[1],face_loc[2],face_loc[3]
+        y1,x2,y2,x1=face_loc[0],face_loc[1],face_loc[2],face_loc[3]
 
         cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,200),4)
-        cv2.putText(frame,name,(x1,y1-10),cv2.FONT_HERSHEY_DUPLEX,1,(1,1,1),2)
+        cv2.putText(frame,name,(x1,y1-10),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,500),2)
 
 
     if not ret or frame is None:
